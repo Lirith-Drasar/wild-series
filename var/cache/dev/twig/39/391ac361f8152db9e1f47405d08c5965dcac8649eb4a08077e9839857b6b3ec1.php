3d94d6dbd3495d2290cde3bcc163a7e629e6750a
Lirith-Drasar <ma.rabouin@laposte.net>
2019-12-09 10:07:32 +0100

    10 episode bug

--- var/cache/dev/twig/39/391ac361f8152db9e1f47405d08c5965dcac8649eb4a08077e9839857b6b3ec1.php
+++ var/cache/dev/twig/39/391ac361f8152db9e1f47405d08c5965dcac8649eb4a08077e9839857b6b3ec1.php
@@ -31,7 +31,7 @@ class __TwigTemplate_4b489aeebc55cb278587965e2a6ae06e7430ad884cca9c2ba8c93957da8
 
     protected function doGetParent(array $context)
     {
-        // line 1
+        // line 2
         return "base.html.twig";
     }
 
@@ -44,7 +44,7 @@ class __TwigTemplate_4b489aeebc55cb278587965e2a6ae06e7430ad884cca9c2ba8c93957da8
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "wild/episode.html.twig"));
 
-        $this->parent = $this->loadTemplate("base.html.twig", "wild/episode.html.twig", 1);
+        $this->parent = $this->loadTemplate("base.html.twig", "wild/episode.html.twig", 2);
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
@@ -54,7 +54,7 @@ class __TwigTemplate_4b489aeebc55cb278587965e2a6ae06e7430ad884cca9c2ba8c93957da8
 
     }
 
-    // line 3
+    // line 4
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -64,45 +64,45 @@ class __TwigTemplate_4b489aeebc55cb278587965e2a6ae06e7430ad884cca9c2ba8c93957da8
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 4
+        // line 5
         echo "    <h1>
         Episode ";
-        // line 5
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["episode"]) || array_key_exists("episode", $context) ? $context["episode"] : (function () { throw new RuntimeError('Variable "episode" does not exist.', 5, $this->source); })()), "number", [], "any", false, false, false, 5), "html", null, true);
+        // line 6
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["episode"]) || array_key_exists("episode", $context) ? $context["episode"] : (function () { throw new RuntimeError('Variable "episode" does not exist.', 6, $this->source); })()), "number", [], "any", false, false, false, 6), "html", null, true);
         echo ": ";
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["episode"]) || array_key_exists("episode", $context) ? $context["episode"] : (function () { throw new RuntimeError('Variable "episode" does not exist.', 5, $this->source); })()), "title", [], "any", false, false, false, 5), "html", null, true);
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["episode"]) || array_key_exists("episode", $context) ? $context["episode"] : (function () { throw new RuntimeError('Variable "episode" does not exist.', 6, $this->source); })()), "title", [], "any", false, false, false, 6), "html", null, true);
         echo "
     </h1>
     <p>
         <b>
             Season ";
-        // line 9
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["season"]) || array_key_exists("season", $context) ? $context["season"] : (function () { throw new RuntimeError('Variable "season" does not exist.', 9, $this->source); })()), "number", [], "any", false, false, false, 9), "html", null, true);
+        // line 10
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["season"]) || array_key_exists("season", $context) ? $context["season"] : (function () { throw new RuntimeError('Variable "season" does not exist.', 10, $this->source); })()), "number", [], "any", false, false, false, 10), "html", null, true);
         echo "
         </b>
         -
         ";
-        // line 12
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["program"]) || array_key_exists("program", $context) ? $context["program"] : (function () { throw new RuntimeError('Variable "program" does not exist.', 12, $this->source); })()), "title", [], "any", false, false, false, 12), "html", null, true);
+        // line 13
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["program"]) || array_key_exists("program", $context) ? $context["program"] : (function () { throw new RuntimeError('Variable "program" does not exist.', 13, $this->source); })()), "title", [], "any", false, false, false, 13), "html", null, true);
         echo "
     </p>
     <p>
         ";
-        // line 15
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["episode"]) || array_key_exists("episode", $context) ? $context["episode"] : (function () { throw new RuntimeError('Variable "episode" does not exist.', 15, $this->source); })()), "synopsis", [], "any", false, false, false, 15), "html", null, true);
+        // line 16
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["episode"]) || array_key_exists("episode", $context) ? $context["episode"] : (function () { throw new RuntimeError('Variable "episode" does not exist.', 16, $this->source); })()), "synopsis", [], "any", false, false, false, 16), "html", null, true);
         echo "
     </p>
     <p>
         <a href=\"";
-        // line 18
-        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("show_season", ["seasonId" => twig_get_attribute($this->env, $this->source, (isset($context["season"]) || array_key_exists("season", $context) ? $context["season"] : (function () { throw new RuntimeError('Variable "season" does not exist.', 18, $this->source); })()), "id", [], "any", false, false, false, 18)]), "html", null, true);
+        // line 19
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("show_season", ["seasonId" => twig_get_attribute($this->env, $this->source, (isset($context["season"]) || array_key_exists("season", $context) ? $context["season"] : (function () { throw new RuntimeError('Variable "season" does not exist.', 19, $this->source); })()), "id", [], "any", false, false, false, 19)]), "html", null, true);
         echo "\">
             Back to season list
         </a>
         -
         <a href=\"";
-        // line 22
-        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("show_program", ["programName" => (isset($context["Title"]) || array_key_exists("Title", $context) ? $context["Title"] : (function () { throw new RuntimeError('Variable "Title" does not exist.', 22, $this->source); })())]), "html", null, true);
+        // line 23
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("show_program", ["programName" => (isset($context["hyphenizedProgramTitle"]) || array_key_exists("hyphenizedProgramTitle", $context) ? $context["hyphenizedProgramTitle"] : (function () { throw new RuntimeError('Variable "hyphenizedProgramTitle" does not exist.', 23, $this->source); })())]), "html", null, true);
         echo "\">
             Back to show details
         </a>
@@ -128,12 +128,13 @@ class __TwigTemplate_4b489aeebc55cb278587965e2a6ae06e7430ad884cca9c2ba8c93957da8
 
     public function getDebugInfo()
     {
-        return array (  105 => 22,  98 => 18,  92 => 15,  86 => 12,  80 => 9,  71 => 5,  68 => 4,  58 => 3,  35 => 1,);
+        return array (  105 => 23,  98 => 19,  92 => 16,  86 => 13,  80 => 10,  71 => 6,  68 => 5,  58 => 4,  35 => 2,);
     }
 
     public function getSourceContext()
     {
-        return new Source("{% extends 'base.html.twig' %}
+        return new Source("
+{% extends 'base.html.twig' %}
 
 {% block body %}
     <h1>
@@ -154,7 +155,7 @@ class __TwigTemplate_4b489aeebc55cb278587965e2a6ae06e7430ad884cca9c2ba8c93957da8
             Back to season list
         </a>
         -
-        <a href=\"{{ path('show_program', {programName: Title}) }}\">
+        <a href=\"{{ path('show_program', {programName: hyphenizedProgramTitle}) }}\">
             Back to show details
         </a>
     </p>

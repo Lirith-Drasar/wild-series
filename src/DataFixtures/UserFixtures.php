ae314eb84048df61286f20af840697d48c8ed2c9
Lirith-Drasar <ma.rabouin@laposte.net>
2020-01-14 10:15:30 +0100

    User Entity and Fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,19 +11,33 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserFixtures extends Fixture
 {
     private $passwordEncoder;
-    
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
     }
-    
     public function load(ObjectManager $manager)
     {
-    $user = new User();
-    
-    $user->setPassword($this->passwordEncoder->encodePassword(
-    $user,
-    'the_new_password'
-    ));
-        }
+        // Création d’un utilisateur de type “auteur”
+        $subscriber = new User();
+        $subscriber->setName("subscriber@monsite.com");
+        $subscriber->setRoles(['ROLE_SUBSCRIBER']);
+        $subscriber->setPassword($this->passwordEncoder->encodePassword(
+            $subscriber,
+            'subscriberpassword'
+        ));
+
+        $manager->persist($subscriber);
+
+        // Création d’un utilisateur de type “administrateur”
+        $admin = new User();
+        $admin->setName('admin@monsite.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'adminpassword'
+        ));
+        $manager->persist($admin);
+        // Sauvegarde des 2 nouveaux utilisateurs :
+        $manager->flush();
     }
+}
